d2ff791a59070ec26c12a8ea7d717294b4d21ab1
linxingming <1274918826@qq.com>
2017-04-27 10:58:59 +0800

    阿萨德

--- MyTest/ViewController.m
+++ MyTest/ViewController.m
@@ -26,16 +26,6 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor lightGrayColor];
-    
-    
-    id dic = @"qweqwe";
-    @try {
-         NSString *test =  [dic objectForKey:@"hh"];
-    } @catch (NSException *exception) {
-        
-    } @finally {
-        
-    }
    
     
     NSString *const XMPP_DOMAIN = @"jianyezuqiu.cn";
